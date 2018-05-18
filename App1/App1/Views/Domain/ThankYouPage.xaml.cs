d79f8f679d1f14bafa9fb63daad7bdac77503502
DanMoll <dani948d@edu.easj.dk>
2018-05-18 10:19:43 +0100

    Made blank pages for navigation.

--- App1/App1/Views/Domain/ThankYouPage.xaml.cs
+++ App1/App1/Views/Domain/ThankYouPage.xaml.cs
@@ -20,9 +20,9 @@ namespace App1.Views.Domain
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DateChoice : Page
+    public sealed partial class ThankYouPage : Page
     {
-        public DateChoice()
+        public ThankYouPage()
         {
             this.InitializeComponent();
         }
