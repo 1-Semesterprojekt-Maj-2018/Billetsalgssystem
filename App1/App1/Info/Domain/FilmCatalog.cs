14c708aa497b0c3f438a9c422d171516f55bbb93
Simon Knude Hansen <simo4563@edu.easj.dk>
2018-05-24 23:15:19 +0200

    Fire film add.

--- App1/App1/Info/Domain/FilmCatalog.cs
+++ App1/App1/Info/Domain/FilmCatalog.cs
@@ -8,9 +8,19 @@ using App1.Info.Base;
 
 namespace App1.Info.Domain
 {
-    public class FilmCatalog : CatalogBase<Film>
+    public class FilmCatalog
     {
         #region Constructor
+
+        static void Catalog()
+        {
+            Film film1 = new Film("Deadpool", 1.59, "Billede");
+            Film film2 = new Film("SOLO star wars", 2.15, "Billede");
+            Film film3 = new Film("Sankt bernhard syndikatet", 1.40, "Billede");
+            Film film4 = new Film("Mit grønland", 1.34, "Billede");
+        }
+
+
 
 
         #endregion
