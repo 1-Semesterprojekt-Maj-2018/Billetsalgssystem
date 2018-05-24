598ad8ad59cd9574f8ea8aa8bb2f5cef34349112
Simon Knude Hansen <simo4563@edu.easj.dk>
2018-05-24 22:42:14 +0200

    Film catalog og base

---
+++ App1/App1/Info/Domain/Film.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Info.Domain
+{
+ public class Film
+    {
+        #region instance fields
+        private string _titel;
+        private double _længde;
+        private string _billede;
+        #endregion
+
+        #region Constuctor
+        public Film(string titel, double længde, string billede)
+        {
+            _titel = titel;
+            _længde = længde;
+            _billede = billede;
+        }
+        #endregion
+
+        #region Properties
+        public string Titel
+        {
+            get { return _titel; }
+        }
+
+        public double Længde
+        {
+            get { return _længde; }
+        }
+
+        public string Billede
+        {
+            get { return _billede; }
+        }
+        #endregion
+    }
+}
---
+++ App1/App1/Info/Domain/FilmCatalog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.AppointmentsProvider;
+using App1.Info.Base;
+
+namespace App1.Info.Domain
+{
+    public class FilmCatalog : CatalogBase
+    {
+        #region Constructor
+
+            
+
+            
+            
+            
+
+        
+
+        #endregion
+    }
+}
