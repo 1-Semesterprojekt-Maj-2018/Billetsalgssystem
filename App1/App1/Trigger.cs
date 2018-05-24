048995b99d6cd88ee337fcad3758d847010e456d
Jonathan Søgaard Petersen <jona173a@edu.easj.dk>
2018-05-24 12:55:48 +0200

    Sat indhold i payment

---
+++ App1/App1/Trigger.cs
@@ -0,0 +1,11 @@
+namespace App1
+{
+    public class Trigger
+    {
+        public object Property
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+    }
+}
