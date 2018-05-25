2d8c52c7fb75341045f42fac0df52dd4e8d9366d
Simon Knude Hansen <simo4563@edu.easj.dk>
2018-05-25 12:25:39 +0200

    ALt muligt

--- App1/App1/Info/Domain/FilmCatalog.cs
+++ App1/App1/Info/Domain/FilmCatalog.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Windows.ApplicationModel.Appointments.AppointmentsProvider;
 using Windows.Media.Audio;
 using Windows.Security.Cryptography.Core;
-using App1.Info.Base;
+
 
 namespace App1.Info.Domain
 {
@@ -21,13 +21,16 @@ namespace App1.Info.Domain
         {
             _films = new List<Film>();
 
-            _films.Add(new Film("Deadpool", 1.59, "Billede"));
-            _films.Add(new Film("Solo star wars", 2.15, "Billede"));
-            _films.Add(new Film("Sankt bernhard syndikatet", 1.40, "Billede"));
-            _films.Add(new Film("Mit grønland", 1.34, "Billede"));
+            _films.Add(new Film("Deadpool", 1.59, "..\\Assets\\Deadpool2.jpg"));
+            _films.Add(new Film("Solo star wars", 2.15, "..\\Assets\\SOlo starwars.jpg"));
+            _films.Add(new Film("Sankt bernhard syndikatet", 1.40, "..\\Assets\\sankt-bernhard-syndikatet.jpg"));
+            _films.Add(new Film("Mit grønland", 1.34, "..\\Assets\\mit-groenland-plakat.jpg"));
 
         }
-
+        public List<Film> Films
+        {
+            get { return _films; }
+        }
 
 
 
