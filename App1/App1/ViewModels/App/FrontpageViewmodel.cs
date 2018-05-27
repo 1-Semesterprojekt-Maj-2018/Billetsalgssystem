7db3a8d5e79683d43ea0036cfd9bbe131c19c27b
Simon Knude Hansen <simo4563@edu.easj.dk>
2018-05-27 20:02:49 +0200

    Prøver at gendanne noget jeg har slettet

---
+++ App1/App1/ViewModels/App/FrontpageViewmodel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BioApp.Annotations;
+using BioApp.Info.Domain;
+using BioApp.Annotations;
+
+namespace BioApp.ViewModels
+{
+    class FrontpageViewmodel : INotifyPropertyChanged
+    {
+        private FilmCatalog _filmCatalog;
+        private Film _selectedFilm;
+
+        public FrontpageViewmodel()
+        {
+            _filmCatalog = new FilmCatalog();
+            _selectedFilm = Filmliste.Films[0];
+        }
+
+        public FilmCatalog Filmliste
+        {
+            get { return _filmCatalog; }
+        }
+
+        public Film SelectedFilm
+        {
+            get { return _selectedFilm; }
+            set
+            {
+                _selectedFilm = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
