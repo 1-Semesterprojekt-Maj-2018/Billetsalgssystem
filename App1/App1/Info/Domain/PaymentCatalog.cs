3ad8534060bed0e902637b4969c34d401b7b70a5
Jonathan Søgaard Petersen <jona173a@edu.easj.dk>
2018-05-28 12:44:27 +0200

    PaymentCatalog oprettet

---
+++ App1/App1/Info/Domain/PaymentCatalog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Info.Domain
+{
+    class PaymentCatalog
+    {
+        private static PaymentCatalog _instance;
+
+        public static PaymentCatalog Instance
+        {
+            get
+            {
+                _instance = _instance == null ? new PaymentCatalog() : _instance;
+                return _instance;
+            }
+        }
+
+        private List<Payment> _payments;
+
+        private PaymentCatalog()
+        {
+            _payments = new List<Payment>();
+
+        }
+        public List<Reservation> Reservations
+        {
+            get { return _reservations; }
+        }
+
+        public void CreateNewReservation()
+        {
+            _reservations.Insert(0, new Reservation(DateTime.Now, 0));
+        }
+
+    }
+}
