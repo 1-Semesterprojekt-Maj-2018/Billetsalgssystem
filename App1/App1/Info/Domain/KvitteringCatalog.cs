7fa1fa56dc2cd5acfb6cbffd7869892f4882ce21
Simon Knude Hansen <simo4563@edu.easj.dk>
2018-05-28 14:37:21 +0200

    Kvittering

---
+++ App1/App1/Info/Domain/KvitteringCatalog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Info.Domain
+{
+    class KvitteringCatalog
+    {
+        private static KvitteringCatalog _instance;
+
+        public static KvitteringCatalog Instance
+        {
+            get
+            {
+                _instance = _instance == null ? new KvitteringCatalog() : _instance;
+                return _instance;
+            }
+        }
+
+        private List<Kvittering> _kvitteringer;
+
+        private KvitteringCatalog()
+        {
+            _kvitteringer = new List<Kvittering>();
+
+        }
+        public List<Kvittering> Payments
+        {
+            get { return _kvitteringer; }
+        }
+
+        public void CreateNewPayment()
+        {
+            _kvitteringer.Insert(0, new Kvittering(("Film"),0,("payment")));
+        }
+    }
+}
---
+++ App1/App1/ViewModels/App/KvitteringViewmodel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Annotations;
+
+namespace App1.ViewModels.App
+{
+    class KvitteringViewmodel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
