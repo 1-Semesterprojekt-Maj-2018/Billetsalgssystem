4ad0f85e8624ef93c51d2871192eaa2cc3c12275
Jonathan Søgaard Petersen <jona173a@edu.easj.dk>
2018-05-28 15:04:13 +0200

    Rettet i paymentcatalog, viewmodel og størrelse på xaml.

--- App1/App1/ViewModels/App/PaymentViewmodel.cs
+++ App1/App1/ViewModels/App/PaymentViewmodel.cs
@@ -20,23 +20,79 @@ namespace App1.ViewModels.App
 
         public string Name
         {
-            get { return PaymentCatalog.Instance.Payments[0].Adresse; }
+            get { return PaymentCatalog.Instance.Payments[0].Navn; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].Navn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string NameAddress
+        {
+            get { return PaymentCatalog.Instance.Payments[0].Adresse;}
             set
             {
                 PaymentCatalog.Instance.Payments[0].Adresse = value;
                 OnPropertyChanged();
             }
         }
-
-        public string Adresse
+        public int NameZipCode
         {
-            get { return PaymentCatalog.Instance.Payments[0].EmailAdresse;}
+            get { return PaymentCatalog.Instance.Payments[0].Postnummer; }
             set
             {
-                PaymentCatalog.Instance.Payments[0].
+                PaymentCatalog.Instance.Payments[0].Postnummer = value;
+                OnPropertyChanged();
             }
         }
 
+        public string NameEmail
+        {
+            get { return PaymentCatalog.Instance.Payments[0].EmailAdresse; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].EmailAdresse = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int NamePhoneNumber
+        {
+            get { return PaymentCatalog.Instance.Payments[0].Telefonnummer; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].Telefonnummer = value;
+                OnPropertyChanged();
+            }
+        }
+        public int NameCardNumber
+        {
+            get { return PaymentCatalog.Instance.Payments[0].Kortnummer; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].Kortnummer = value;
+                OnPropertyChanged();
+            }
+        }
+        public int NameCardExpire
+        {
+            get { return PaymentCatalog.Instance.Payments[0].kortUdløb; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].kortUdløb = value;
+                OnPropertyChanged();
+            }
+        }
+        public int NameCvv
+        {
+            get { return PaymentCatalog.Instance.Payments[0].Cvv; }
+            set
+            {
+                PaymentCatalog.Instance.Payments[0].Cvv = value;
+                OnPropertyChanged();
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
